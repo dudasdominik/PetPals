4ebc531e9e0a202dd8bf03285623c26add213177
dudasdominik <dudasdominik3@gmail.com>
2023-10-02 09:38:38 +0200

    add MessageModel.cs

---
+++ Models/MessageModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetPals.Models;
+[Table("message")]
+public class MessageModel
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    public UserModel Sender { get; set; }
+    public UserModel Recipient { get; set; }
+    public string Body { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public ICollection<PostModel> Photos { get; set; } = new List<PostModel>();
+    public bool IsRead { get; set; } = false;
+}
