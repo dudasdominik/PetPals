56509bc71d5c95cc82dd2b4c050268b78d09cc64
dudasdominik <dudasdominik3@gmail.com>
2023-10-02 09:36:34 +0200

    add PostTagModel.cs

---
+++ Models/PostTagModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PetPals.Models;
+
+public class PostTagModel
+{
+    public Guid PostId { get; set; }
+    public PostModel Post { get; set; }
+
+    public Guid TagId { get; set; }
+    public TagModel Tag { get; set; }
+}
