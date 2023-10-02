99d17fb5013694e3496555db580ca51411fd690b
dudasdominik <dudasdominik3@gmail.com>
2023-10-02 09:39:14 +0200

    add CommentModel.cs

---
+++ Models/CommentModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetPals.Models;
+[Table("comment")]
+public class CommentModel
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    public string Body { get; set; }
+    public UserModel Sender { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public PostModel ToPost { get; set; }
+    public Guid ParentCommentId { get; set; }
+    public ICollection<CommentModel> Replies { get; set; } = new List<CommentModel>();
+    public bool IsReplyTo { get; set; } = false;
+}
