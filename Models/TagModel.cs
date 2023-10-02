a1c624d4bc85bcb56b01a845f394178c069e32d5
dudasdominik <dudasdominik3@gmail.com>
2023-10-02 09:36:25 +0200

    add TagModel.cs

---
+++ Models/TagModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetPals.Models;
+public class TagModel
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+    public string TagName { get; set; }
+    public ICollection<PostTagModel> PostTags { get; set; } = new List<PostTagModel>();
+}
