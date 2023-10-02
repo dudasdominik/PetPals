14e503937f1c0c79d43b26714fe46ba00711b738
dudasdominik <dudasdominik3@gmail.com>
2023-10-02 09:40:10 +0200

    add PetPalsContext.cs

---
+++ Models/PetPalsContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PetPals.Models;
+
+public class PetPalsContext : DbContext
+{
+    public PetPalsContext(DbContextOptions<PetPalsContext> contextOptions) : base(contextOptions)
+    {
+        
+    }
+
+    public DbSet<ChatRoomModel> ChatRoomModels { get; set; }
+    public DbSet<CommentModel> CommentModels { get; set; }
+    public DbSet<MessageModel> MessageModels { get; set; }
+    public DbSet<NotificationModel> NotificationModels { get; set; }
+    public DbSet<PostModel> PostModels { get; set; }
+    public DbSet<UserModel> UserModels { get; set; }
+    public DbSet<PhotoModel> PhotoModels { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<PostTagModel>()
+            .HasKey(pt => new { pt.PostId, pt.TagId });
+
+        modelBuilder.Entity<PostTagModel>()
+            .HasOne(pt => pt.Post)
+            .WithMany(p => p.Tags)
+            .HasForeignKey(pt => pt.PostId);
+
+        modelBuilder.Entity<PostTagModel>()
+            .HasOne(pt => pt.Tag)
+            .WithMany(t => t.PostTags)
+            .HasForeignKey(pt => pt.TagId);
+
+
+        modelBuilder.Entity<PostModel>()
+            .HasMany(p => p.Likes)
+            .WithMany(u => u.LikedPosts)
+            .UsingEntity(j => j.ToTable("PostUserLikes"));
+        
+        modelBuilder.Entity<PostModel>()
+            .HasMany(p => p.Saves)
+            .WithMany(u => u.SavedPosts)
+            .UsingEntity(j => j.ToTable("PostUserSaves"));
+       
+        modelBuilder.Entity<PostModel>()
+            .HasOne(p => p.Sender)
+            .WithMany(u => u.OwnPosts)
+            .HasForeignKey(p => p.SenderId);
+    }
+}
